19286e6d14e08ca64e83ee39eeea231c5ae95b80
Camilo Manzur <camiloms1060@gmail.com>
2023-09-09 00:21:32 +0000

    advanced the dbt modeling

---
+++ dbt/models/staging/players_stats_all.sql
@@ -0,0 +1,84 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='view',
+        partition_by={
+            "field": "DATE_TRUNC(GAME_DATE, YEAR)"
+        },
+        cluster_by=['TEAM_ID', 'PLAYER_ID']
+    )
+}}
+
+
+select
+cast (PLAYER_ID as integer) as PLAYER_ID,
+cast (TEAM_ID as integer) as TEAM_ID,
+cast (GAME_ID as integer) as GAME_ID,
+cast (GAME_DATE as date) as GAME_DATE,
+cast (WL as string) as WL,
+cast (MIN as decimal) as MIN,
+cast (FGM as integer) as FGM,
+cast (FGA as integer) as FGA,
+cast (FG_PCT as decimal) as FG_PCT,
+cast (FG3M as integer) as FG3M,
+cast (FG3A as integer) as FG3A,
+cast (FG3_PCT as decimal) as FG3_PCT,
+cast (FTM as integer) as FTM,
+cast (FTA as integer) as FTA,
+cast (FT_PCT as decimal) as FT_PCT,
+cast (OREB as integer) as OREB,
+cast (DREB as integer) as DREB,
+cast (REB as integer) as REB,
+cast (AST as integer) as AST,
+cast (TOV as integer) as TOV,
+cast (STL as integer) as STL,
+cast (BLK as integer) as BLK,
+cast (BLKA as integer) as BLKA,
+cast (PF as integer) as PF,
+cast (PFD as integer) as PFD,
+cast (PTS as integer) as PTS,
+cast (PLUS_MINUS as integer) as PLUS_MINUS
+from {{ source('staging','players_game_stats_current_season') }} 
+
+UNION ALL
+
+select
+cast (PLAYER_ID as integer) as PLAYER_ID,
+cast (TEAM_ID as integer) as TEAM_ID,
+cast (GAME_ID as integer) as GAME_ID,
+cast (GAME_DATE as date) as GAME_DATE,
+cast (WL as string) as WL,
+cast (MIN as decimal) as MIN,
+cast (FGM as integer) as FGM,
+cast (FGA as integer) as FGA,
+cast (FG_PCT as decimal) as FG_PCT,
+cast (FG3M as integer) as FG3M,
+cast (FG3A as integer) as FG3A,
+cast (FG3_PCT as decimal) as FG3_PCT,
+cast (FTM as integer) as FTM,
+cast (FTA as integer) as FTA,
+cast (FT_PCT as decimal) as FT_PCT,
+cast (OREB as integer) as OREB,
+cast (DREB as integer) as DREB,
+cast (REB as integer) as REB,
+cast (AST as integer) as AST,
+cast (TOV as integer) as TOV,
+cast (STL as integer) as STL,
+cast (BLK as integer) as BLK,
+cast (BLKA as integer) as BLKA,
+cast (PF as integer) as PF,
+cast (PFD as integer) as PFD,
+cast (PTS as integer) as PTS,
+cast (PLUS_MINUS as integer) as PLUS_MINUS
+from {{ source('staging','players_game_stats_past') }}
+
+
+{% if var('is_test_run', default=true) %}
+    limit 500
+{% endif %}
